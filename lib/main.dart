d5c91c55e99a228fc34fae1457250a7250474987
rifqi <rifqidias6@gmail.com>
2022-08-27 12:05:41 +0700

    latest :fire::fire::fire::fire:

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebasefittracker/firebase_options.dart';
-import 'package:firebasefittracker/page/google_sign_in_page.dart';
+import 'package:firebasefittracker/main_page.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter fit-Tracker',
-      home: GoogleSignInPage(),
+      home: MainPage(),
     );
   }
 }
